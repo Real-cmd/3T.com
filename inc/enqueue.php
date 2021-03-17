f5301c002488deb7d026b2fc4ccb31d93f6742d1
Me2du <sarderreal00@gmail.com>
2021-03-17 13:13:44 +0600

    Initial State of my code(Test)

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,10 +6,16 @@
  * ================================
  */
 
-function theme3T_load_admin_script(){
+function theme3T_load_admin_scripts($hook){
+if ('toplevel_page_theme_3T.com' == $hook){
 
-    wp_enqueue_style('theme3T_admin', get_template_directory_uri() . '/css/theme3T.admin.css', array(), '1.0.0', 'all');
+
+    wp_register_style('theme3T_admin', get_template_directory_uri() . '/css/theme3T.admin.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('theme3T_admin');
     //wp_enqueue_style('theme3T_admin');
+    }else{
+        return;
+    }
 }
 
-add_action('admin_enqueue_scripts','theme3T_load_admin_script');
+add_action('admin_enqueue_scripts','theme3T_load_admin_scripts');
--- inc/template/theme_admin_template.php
+++ inc/template/theme_admin_template.php
@@ -1,5 +1,23 @@
 <h1>3T.com Sidebar Options</h1>
 <?php settings_errors(); ?>
+<?php
+$firstName = esc_attr(get_option('first_name'));
+$lastName = esc_attr(get_option('last_name'));
+$fullName = $firstName . ' ' . $lastName;
+$userDes = esc_attr(get_option('user_description'));
+?>
+
+<div class="theme3T_sidebar_preview">
+    <div class="theme3T_sidebar">
+        <h1 class="theme3T_username"><?php print $fullName ?></h1>
+        <h2 class="theme3T_description"><?php print $userDes ?></h2>
+        <div class="icon_wrapper">
+
+        </div>
+    </div>
+</div>
+
+
 <form action="options.php" method="post">
     <?php settings_fields('theme3T-set-register') ?>
     <?php do_settings_sections('theme_3T.com') ?>
