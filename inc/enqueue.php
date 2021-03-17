56fb36b99011ae20ee7f5581f48a8ee6efe09043
Me2du <sarderreal00@gmail.com>
2021-03-17 12:32:06 +0600

    Initial State of my code(Test)

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,8 +8,8 @@
 
 function theme3T_load_admin_script(){
 
-        wp_register_style('theme3T_admin', get_template_directory_uri() . '/css/theme3T.admin.css', array(), '1.0.0', 'all');
-        wp_enqueue_style('theme3T_admin');
+    wp_enqueue_style('theme3T_admin', get_template_directory_uri() . '/css/theme3T.admin.css', array(), '1.0.0', 'all');
+    //wp_enqueue_style('theme3T_admin');
 }
 
 add_action('admin_enqueue_scripts','theme3T_load_admin_script');
